3eedf94ab9e9366e45934129b1ed583a1d474d56
Busra Al Nakhlani <musictakesyouback@gmail.com>
2016-11-09 21:03:02 +0100

    ex5.rb

--- ex5.rb
+++ ex5.rb
@@ -5,6 +5,13 @@ I want someone with bad teeth..."'
 puts "Zoe Leonard"
 puts "1992"
 #argument loop:
-ARGV.each do|line|
-  puts "#{line}"
+# ARGV.each do|line|
+#   puts "#{line}"
+# end
+# puts "You added #{ARGV.length} lines to the poem."
+def name
+puts "Who are you?"
+input = gets.chomp
+puts "Zoe Leonard & #{input}"
 end
+name
