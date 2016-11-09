7c76ba18011afd51b0f24b5cfa229948792a046f
Busra Al Nakhlani <musictakesyouback@gmail.com>
2016-11-09 19:35:51 +0100

    ex2.rb

---
+++ ex2.rb
@@ -0,0 +1,13 @@
+puts "\"I want a dyke for president" #thin back slash is helping us to escape the quotes
+puts "..."
+puts "I want a Black woman for president"
+puts "I want someone with bad teeth...\""
+puts "Zoe Leonard"
+puts "1992"
+#here is another way to do it:
+# puts '"I want a dyke for president
+# ...
+# I want a Black woman for president
+# I want someone with bad teeth..."'
+# puts "Zoe Leonard"
+# puts "1992"
