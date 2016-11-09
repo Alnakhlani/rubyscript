2eb82e3c3a5016f48eafce7a706059db8506fcd0
Busra Al Nakhlani <musictakesyouback@gmail.com>
2016-11-09 20:15:57 +0100

    ex5.rb

---
+++ ex5.rb
@@ -0,0 +1,10 @@
+puts '"I want a dyke for president
+...
+I want a Black woman for president
+I want someone with bad teeth..."'
+puts "Zoe Leonard"
+puts "1992"
+#argument loop:
+ARGV.each do|line|
+  puts "#{line}"
+end
